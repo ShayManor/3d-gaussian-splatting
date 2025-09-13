7d605cfa12a93f69de96815a5705ced974424ea0
shaymanor <shay.manor@gmail.com>
2025-09-13 22:11:20 +0000

    chore: added train command.

--- src/run.sh
+++ src/run.sh
@@ -16,9 +16,14 @@ rm -rf src/content/outputs/scene/splatfacto/*
 
 # Trains gaussian splatting model
 ns-train splatfacto --data src/content/scene \
---output-dir src/content/outputs
+  --output-dir /content/outputs \
+  --viewer.websocket-host 127.0.0.1 \
+  --viewer.websocket-port 7007
+
+#  Then on laptop: ssh -p 37937 -N -L 7007:0.0.0.0:7007 root@ssh5.vast.ai
 
 # Convert format to viewable form
 ns-export gaussian-splat \
 --load-config src/content/outputs/scene/splatfacto/2025-09-13_201337/config.yml \
 --output-dir src/content/exports
+
